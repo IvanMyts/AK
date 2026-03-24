38ef0bdc32ac5d1dd11d90174146a5f8bcd7be45
Максим Кац <128629593+RustyMoose1@users.noreply.github.com>
2026-03-24 11:20:12 +0300

    Add files

--- big_to_little_endian_SHORTEST.s
+++ big_to_little_endian_SHORTEST.s
@@ -1,24 +1,17 @@
     .data
-
-input_adr:       .word  0x80
 input:           .word  0x0
 output:          .word  0x0
-output_adr:      .word  0x84
 const_1:         .word  0xFF
 const_2:         .word  0xFF00
 const_3:         .word  0xFF0000
 const_4:         .word  0xFF000000
-
 const_5:         .word  0x8
 const_6:         .word  0x18
 
     .text
-    .org 0x100
-
 _start:
 
-    load_addr    input_adr
-    load_acc
+    load_addr    0x80
     store        input
     shiftl       const_6
     and          const_4
@@ -40,5 +33,5 @@ _start:
     shiftr       const_6
     and          const_1
     add          output
-    store_ind    output_adr
+    store_addr   0x84
     halt
